383c270ca42632cc9b059dfe9fb9a6702c64550e
Hakris <hakris001@gmail.com>
2022-06-17 21:29:13 -0500

    Actualizando el proyecto 1.0

--- src/models/InicioSesion.java
+++ src/models/InicioSesion.java
@@ -2,6 +2,7 @@
 package models;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -12,6 +13,9 @@ public class InicioSesion extends javax.swing.JFrame {
     DefaultTableModel modelo = new DefaultTableModel();
     public InicioSesion() {
         initComponents();
+        jbtn_Tapar.setVisible(false);
+        this.setLocationRelativeTo(null);
+        
     }
 
     @SuppressWarnings("unchecked")
@@ -23,11 +27,13 @@ public class InicioSesion extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jtxt_Usuario = new javax.swing.JTextField();
-        jtxt_Contrasenia = new javax.swing.JTextField();
         jbtn_Acceder = new javax.swing.JButton();
         jLabel4 = new javax.swing.JLabel();
         jlbl_Aqui = new javax.swing.JLabel();
         jbtn_AccesoBiometrico = new javax.swing.JButton();
+        jpsw_Contrasenia = new javax.swing.JPasswordField();
+        jbtn_Ver = new javax.swing.JButton();
+        jbtn_Tapar = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -46,10 +52,7 @@ public class InicioSesion extends javax.swing.JFrame {
         jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(27, 177, -1, -1));
 
         jtxt_Usuario.setFont(new java.awt.Font("SansSerif", 0, 24)); // NOI18N
-        jPanel1.add(jtxt_Usuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(169, 118, 340, -1));
-
-        jtxt_Contrasenia.setFont(new java.awt.Font("SansSerif", 0, 24)); // NOI18N
-        jPanel1.add(jtxt_Contrasenia, new org.netbeans.lib.awtextra.AbsoluteConstraints(169, 174, 340, -1));
+        jPanel1.add(jtxt_Usuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(169, 118, 300, -1));
 
         jbtn_Acceder.setFont(new java.awt.Font("SansSerif", 0, 24)); // NOI18N
         jbtn_Acceder.setText("Acceder");
@@ -79,6 +82,25 @@ public class InicioSesion extends javax.swing.JFrame {
         jbtn_AccesoBiometrico.setEnabled(false);
         jPanel1.add(jbtn_AccesoBiometrico, new org.netbeans.lib.awtextra.AbsoluteConstraints(250, 260, -1, 40));
 
+        jpsw_Contrasenia.setFont(new java.awt.Font("SansSerif", 0, 24)); // NOI18N
+        jPanel1.add(jpsw_Contrasenia, new org.netbeans.lib.awtextra.AbsoluteConstraints(170, 180, 300, -1));
+
+        jbtn_Ver.setFont(new java.awt.Font("SansSerif", 0, 18)); // NOI18N
+        jbtn_Ver.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbtn_VerActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jbtn_Ver, new org.netbeans.lib.awtextra.AbsoluteConstraints(480, 180, 40, 40));
+
+        jbtn_Tapar.setFont(new java.awt.Font("SansSerif", 0, 18)); // NOI18N
+        jbtn_Tapar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jbtn_TaparActionPerformed(evt);
+            }
+        });
+        jPanel1.add(jbtn_Tapar, new org.netbeans.lib.awtextra.AbsoluteConstraints(480, 180, 40, 40));
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -94,12 +116,32 @@ public class InicioSesion extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jbtn_AccederActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtn_AccederActionPerformed
-        comprobarDatos();
+        if(comprobarDatos()){
+            Principal pp = new Principal();
+            pp.show();
+            this.dispose();
+        }
     }//GEN-LAST:event_jbtn_AccederActionPerformed
 
     private void jlbl_AquiMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jlbl_AquiMouseClicked
-        //Crear cuenta
+        CrearCuenta cc = new CrearCuenta();
+        cc.show();
+        this.dispose();
     }//GEN-LAST:event_jlbl_AquiMouseClicked
+
+    private void jbtn_VerActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtn_VerActionPerformed
+        jpsw_Contrasenia.setEchoChar((char) 0);
+        jbtn_Ver.setVisible(false);
+        jbtn_Tapar.setVisible(true);
+        jpsw_Contrasenia.requestFocus();
+    }//GEN-LAST:event_jbtn_VerActionPerformed
+
+    private void jbtn_TaparActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbtn_TaparActionPerformed
+        jpsw_Contrasenia.setEchoChar(('*'));
+        jbtn_Tapar.setVisible(false);
+        jbtn_Ver.setVisible(true);
+        jpsw_Contrasenia.requestFocus();
+    }//GEN-LAST:event_jbtn_TaparActionPerformed
 
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
@@ -144,8 +186,10 @@ public class InicioSesion extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel1;
     private javax.swing.JButton jbtn_Acceder;
     private javax.swing.JButton jbtn_AccesoBiometrico;
+    private javax.swing.JButton jbtn_Tapar;
+    private javax.swing.JButton jbtn_Ver;
     private javax.swing.JLabel jlbl_Aqui;
-    private javax.swing.JTextField jtxt_Contrasenia;
+    private javax.swing.JPasswordField jpsw_Contrasenia;
     private javax.swing.JTextField jtxt_Usuario;
     // End of variables declaration//GEN-END:variables
 
@@ -173,8 +217,45 @@ public class InicioSesion extends javax.swing.JFrame {
         }
     }
 
-    private void comprobarDatos() {
-        
+    private boolean comprobarDatos() {
+        if (jtxt_Usuario.getText().isEmpty() || jtxt_Usuario.getText() == "") {
+            JOptionPane.showMessageDialog(null, "Ingrese un nombre de usuario");
+            jtxt_Usuario.requestFocus(); //y le damos focus
+        } else if (jpsw_Contrasenia.getText().isEmpty() || jpsw_Contrasenia.getText() == "") { //Comprobamos que el usuario ingreso la pass
+            JOptionPane.showMessageDialog(null, "Ingrese la contraseña");
+            jpsw_Contrasenia.requestFocus(); //sino le damos focus
+        } else {
+            //String nuevoPass = hash.shal(jpsw_Pass.getText()); //encriptamos la password
+            String nuevoPass =(jpsw_Contrasenia.getText());
+            try {
+                conexion cc = new conexion();
+                Connection cn = cc.conectar();
+                String sql = "";
+                sql = "select contrasena_reg FROM usuarios WHERE usuario_reg = ?";
+                PreparedStatement psd = cn.prepareStatement(sql);
+                psd.setString(1, jtxt_Usuario.getText());
+                ResultSet rs = psd.executeQuery();
+                if (rs.next()) {
+                    if (nuevoPass.equals(rs.getString("contrasena_reg"))) {
+                        return true;
+                    }else{
+                        JOptionPane.showMessageDialog(null, "Contraseña incorrecta");
+                    }
+                }else{
+                    JOptionPane.showMessageDialog(null, "Usuario incorrecto");
+                }
+                return false;
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, ex);
+                return false;
+            }
+
+        }
+        return false;
+    }
+
+    private void unlogUsuario() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
 
